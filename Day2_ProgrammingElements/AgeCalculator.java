cf5569af607dfebc139c6fe62547e5a299009369
yash agrawal <yashagr12345678@gmail.com>
2025-06-12 11:06:00 +0530

    Added Day2_ProgrammingElements

---
+++ Day2_ProgrammingElements/AgeCalculator.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class AgeCalculator {
+
+	public static void main(String[] args) {
+		int birthyear = 2000;
+		int currentyear = 2024;
+		int age = currentyear - birthyear;
+		System.out.println("  Harry's age in 2024 is " + age);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
